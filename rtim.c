1b5f3bf683c01e373f1a103b0cfd5bcb18d09a74
LydiaMarieWilliamson <60720435+LydiaMarieWilliamson@users.noreply.github.com>
2021-09-24 16:17:39 -0500

    Minor changes in the comments.
    ... to further align with the 2021 C translation.

---
+++ rtim.c
@@ -0,0 +1,45 @@
+// Revisions Copyright (c) 2021, Darth Spectra (Lydia Marie Williamson).
+#include "extern.h"
+#include "common.h"
+
+#if 1
+//(@) Cheat, cheat! We make a ‟fallback” using a hack from the other repository.
+#include <time.h>
+void ITime(int HMS[3]) {
+   time_t Now; time(&Now);
+   struct tm *HereNow = localtime(&Now);
+   HMS[0] = HereNow->tm_hour, HMS[1] = HereNow->tm_min, HMS[2] = HereNow->tm_sec;
+}
+#else
+extern int /* Subroutine */ itime(int *);
+#   define ITime(HMS) ((void)itime(HMS))
+#endif
+
+void intime(int * hours, int * minutes, int * seconds) {
+   int tarray[3];
+
+// 	CALL GETTIM(HOURS, MINUTES, SECONDS, HUNDREDTHS)
+   ITime(tarray);
+   *hours = tarray[0];
+   *minutes = tarray[1];
+   *seconds = tarray[2];
+}
+
+void inirnd(int seed) {
+   random_.seedy = seed;
+}
+
+int rnd(int maxval) {
+// System generated locals
+   int ret_val;
+
+   random_.seedy = random_.seedy * 1059 + 17 & 2147483647;
+   ret_val = random_.seedy / 100 % maxval;
+   return ret_val;
+}
+
+void exit_(void) {
+// NO "CALL EXIT" HERE
+   char *Msg = "";
+   StopS(Msg, sizeof Msg);
+}
